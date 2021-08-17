144d81d574f6faf9bae85b3660028a617e897504
Rybintseva <rybintseva.t@gmail.com>
2021-08-17 18:29:14 +0300

    Fixes token and driver fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -45,42 +45,16 @@ def user_data():
 @pytest.fixture(scope='session')
 def token():
     api = ApiClient()
-    location = 'v1/login'
-    result = api.request('POST', location, HEADERS).json()['token']
-    return result
+    token = api.get_token()
+    return token
 
 
-@pytest.fixture(scope='function')
-def test_reports_dir(request):
-    test_name = request._pyfuncitem.nodeid.replace('/', '_').replace(':', '_')
-    reports_dir = os.path.join(HOME_PATH, 'reports', test_name)
-    os.makedirs(reports_dir)
-    return reports_dir
-
-
-@pytest.fixture(scope='class')
-def driver(browser, request):
+@pytest.fixture()
+def driver(browser):
     driver = get_driver(browser)
     driver.implicitly_wait(10)
     driver.set_window_size(1920, 1080)
-
-    failed_tests_count = request.session.testsfailed
-
     yield driver
-
-    if request.session.testsfailed > failed_tests_count:
-        allure.attach(driver.get_screenshot_as_png(), name='screenshot', attachment_type=allure.attachment_type.PNG)
-        screenshot_file = os.path.join(test_reports_dir, 'failure.png')
-        driver.get_screenshot_as_file(screenshot_file)
-        allure.attach.file(screenshot_file, 'failure.png', attachment_type=allure.attachment_type.PNG)
-        browser_logfile = os.path.join(test_reports_dir, 'browser.log')
-        with open(browser_logfile, 'w') as f:
-            for i in driver.get_log('browser'):
-                f.write(f"{i['level']} - {i['source']}\n{i['message']}\n\n")
-
-        with open(browser_logfile, 'r') as f:
-            allure.attach(f.read(), 'browser.log', attachment_type=allure.attachment_type.TEXT)
-
     driver.quit()
 
 
